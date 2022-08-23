0954beb6a2a28e0f103c1f24cca2c97d04996a8c
huzeyfebostan <huzeyfebostan99@gmail.com>
2022-08-23 17:27:28 +0300

    controller

--- controllers/authController.go
+++ controllers/authController.go
@@ -17,7 +17,7 @@ func Register(c *fiber.Ctx) error {
 	if data["password"] != data["password_confirm"] {
 		c.Status(400)
 		return c.JSON(fiber.Map{
-			"message": "Şifre yanlış",
+			"message": "passwords do not match",
 		})
 	}
 
@@ -31,6 +31,34 @@ func Register(c *fiber.Ctx) error {
 	}
 
 	database.DB.Create(&user)
+
+	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "not found",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
 
 	return c.JSON(user)
 }
